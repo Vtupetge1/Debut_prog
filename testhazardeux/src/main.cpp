198e40af74d5e81e53ba91f505273c6e6bf14020
virgile.tupet <virgile.tupet@universite-paris-saclay.fr>
2022-12-28 14:58:55 +0100

    partie obligatoire carte moteur

--- testhazardeux/src/main.cpp
+++ testhazardeux/src/main.cpp
@@ -9,19 +9,108 @@
 #include "my_header.h"
 #include <iostream>
 #include <string>
+PwmOut MotG(PA_7);
+PwmOut MotD(PA_6);
+Timer Chrono;
 
-// JAMAIS ICI
-
+DigitalIn BP0(PA_9, PullUp);
+DigitalIn BP1(PA_10, PullUP);
+DigitalIn BP2(PB_0, PullUP);
+DigitalIn BP3(PB_7, PullUP);
 
 int main() {
-    // TOUJOURS ICI
-float valeurB;
-float valeureF;
-valeureF = calculateur (valeurB);
-std::cout << valeureF;
 
-return 0;
+    MotG.period(1);
+    MotD.period(1);
+    Chrono.start;
+
+while(1)
+{
+    if(!BP0)
+    {
+        MotD.write(0);
+        MotG.write(0);
+    }
+    else if(!BP1)
+    {
+        while(!BP1)
+        {
+            Chrono.reset();
+            if (Chrono.read_ms() < 500)
+            {
+                MotD.write(0,25);
+            }
+        else if (Chrono.read_ms() > 500 && Chrono.read_ms() < 1000)
+            {
+                MotD.write(0,5);
+            }
+        else if (Chrono.read_ms() > 1000 && Chrono.read_ms() < 1500)
+            {
+                MotD.write(0,75);
+            }
+        else if (Chrono.read_ms() > 1500)
+            {
+                MotD.write(1);
+            }
+
+        }
+    }
+    else if(!BP2)
+    {
+        while(!BP2)
+        {
+            Chrono.reset();
+            if (Chrono.read_ms() < 500)
+            {
+                MotD.write(0,25);
+                MotG.write(0,25);
+            }
+        else if (Chrono.read_ms() > 500 && Chrono.read_ms() < 1000)
+            {
+                MotD.write(0,5);
+                MotG.write(0,5);
+            }
+        else if (Chrono.read_ms() > 1000 && Chrono.read_ms() < 1500)
+            {
+                MotD.write(0,75);
+                MotG.write(0,75);
+            }
+        else if (Chrono.read_ms() > 1500)
+            {
+                MotD.write(1);
+                MotG.write(1);
+            }
+
+        }
+    }
+    else if(!BP3)
+    {
+        while(!BP3)
+        {
+            Chrono.reset();
+            if (Chrono.read_ms() < 500)
+            {
+                MotG.write(0,25);
+            }
+        else if (Chrono.read_ms() > 500 && Chrono.read_ms() < 1000)
+            {
+                MotG.write(0,5);
+            }
+        else if (Chrono.read_ms() > 1000 && Chrono.read_ms() < 1500)
+            {
+                MotG.write(0,75);
+            }
+        else if (Chrono.read_ms() > 1500)
+            {
+                MotG.write(1);
+            }
+
+        }
+    }
 }
+
+}
+
 
 // type nom(typeparam nomparam, typeparam nomparam);
 // typedef def nom
