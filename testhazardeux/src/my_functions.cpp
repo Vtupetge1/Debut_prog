0c1e1544049e19af018435518ad301b4a0dcb987
virgile.tupet <virgile.tupet@universite-paris-saclay.fr>
2022-12-28 15:02:03 +0100

    oublie de fichier a effacer

--- testhazardeux/src/my_functions.cpp
+++ testhazardeux/src/my_functions.cpp
@@ -4,15 +4,4 @@
 #include "my_header.h"
 #include <iostream>
 
-float resultat;
-
-float calculateur (float(valeurDeBase))
-{
-    std::cout << "taper une valeur a virgule :\n ";
-    std::cin >> valeurDeBase ;
-    resultat = valeurDeBase * valeurDeBase * 3.14;
-
-    return resultat;
-}
-
 
